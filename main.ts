ab3d060ec8740a434a555839a97c3f7f709e3436
Peter Rentz <peter.rentz@nytimes.com>
2024-05-18 18:01:02 -0400

    random numbers and ghost

--- main.ts
+++ main.ts
@@ -1,57 +1,29 @@
-let letter = 0
+let plant = 0
 input.onButtonPressed(Button.A, function () {
-    if (letter == 0) {
+    if (plant == 0) {
         basic.showIcon(IconNames.Yes)
-        music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(932, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     } else {
         basic.showIcon(IconNames.No)
-        music.play(music.tonePlayable(131, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(196, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (plant == 0) {
+        basic.showIcon(IconNames.Yes)
+        music.play(music.tonePlayable(932, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    } else {
+        basic.showIcon(IconNames.No)
+        music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
 })
 basic.forever(function () {
-    letter = 1
-    basic.showLeds(`
-        # # # # .
-        # . . # .
-        # # # # .
-        # . . . .
-        # . . . .
-        `)
-    basic.pause(200)
-    letter = 0
-    basic.showLeds(`
-        # # # # .
-        # . . . .
-        # # # . .
-        # . . . .
-        # # # # #
-        `)
-    basic.pause(200)
-    letter = 1
-    basic.showLeds(`
-        # # # # #
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
-    basic.pause(200)
-    letter = 0
-    basic.showLeds(`
-        # # # # .
-        # . . . .
-        # # # . .
-        # . . . .
-        # # # # #
-        `)
-    basic.pause(200)
-    letter = 1
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
-    basic.pause(100)
+    plant = randint(0, 10)
+    if (plant == 0) {
+        basic.showIcon(IconNames.Ghost)
+        basic.pause(500)
+    } else {
+        basic.showNumber(plant)
+        basic.pause(500)
+    }
 })
